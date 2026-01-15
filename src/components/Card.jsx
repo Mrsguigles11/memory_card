2d57e4eb8413019fc2705711aeb8dd2931e3a284
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-01-15 20:10:35 +0000

    add some styling to cards and a font to whole page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import "../styles/Card.css"
 
 export function Card({name, image}) {
     return (
-        <div className="card_container">
-            <div>{name}</div>
+        <div className="card">
             <img src={image} alt={name + "_image"}/>
+            <div>{String(name).charAt(0).toUpperCase() + String(name).slice(1)}</div>
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export function Header({turnCount, highScore}) {
 
     return (   
         <div className="header_container">
-            <h1>Pokemon Memory Card!</h1>
+            <h1>Pokemon Memory Card</h1>
             <div className="score_container">
                 <div>Score: {turnCount}</div>
                 <div>High Score: {highScore}</div>
