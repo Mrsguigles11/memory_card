1645eb2391357bd77e07f7b8063974e736e2da75
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-01-18 16:21:04 +0000

    add an effect to GameEndOverlay so that the user cant scroll if the overlay is visible

--- src/components/GameEndOverlay.jsx
+++ src/components/GameEndOverlay.jsx
@@ -1,17 +1,35 @@
-import "../styles/GameEndOverlay.css"
+import "../styles/GameEndOverlay.css";
+import { useEffect } from "react";
 
-export function GameEndOverlay({result, score, reset}) {
+export function GameEndOverlay({ result, score, reset }) {
+  
+    useEffect(() => {
+    if (result !== "") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [result]);
 
-    return (
-        <div className={result != "" ? "overlay visible" : "overlay"}>
-            <div className="overlay_card">
-                <div className={result === "game over" ? "game_over visible" : "game_over"}>
-                    <h1>Game Over!</h1>
-                    Score: {score}
-                </div>
-                <h1 className={result === "game won" ? "game_won visible": "game_won"}>You Win!</h1>
-                <button onClick={() => reset(result === "game over" ? false : true)}>Reset</button>
-            </div>
+  return (
+    <div className={result != "" ? "overlay visible" : "overlay"}>
+      <div className="overlay_card">
+        <div
+          className={result === "game over" ? "game_over visible" : "game_over"}
+        >
+          <h1>Game Over!</h1>
+          Score: {score}
         </div>
-    )
+        <h1 className={result === "game won" ? "game_won visible" : "game_won"}>
+          You Win!
+        </h1>
+        <button onClick={() => reset(result === "game over" ? false : true)}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
